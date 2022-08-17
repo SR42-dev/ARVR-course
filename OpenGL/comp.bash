c5f35de8f563a0b215117e69ddc1865dc50fb4ac
Sriram Radhakrishna <sriram.radhakrishna42@gmail.com>
2022-08-17 14:34:16 +0530

    Colors and point loops.

---
+++ OpenGL/comp.bash
@@ -0,0 +1,1 @@
+g++ projectionMatrix.cpp -lglut -lGL -lGLU -lGLEW
--- OpenGL/projectionMatrix.cpp
+++ OpenGL/projectionMatrix.cpp
@@ -4,8 +4,18 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT); // clear the window
 
+    glBegin(GL_POINTS);
+
     glColor3f(1, 0, 0);
-    glutWireTeapot(0.25);
+    for(int i = 0; i < 250; i++) glVertex2f(i, i);
+
+    glColor3f(0, 1, 0);
+    for(int i = 250; i < 500; i++) glVertex2f(i, (500 - i));
+
+    glColor3f(0, 0, 1);
+    for(int i = 500; i > 0; i--) glVertex2f(i, 5);
+
+    glEnd();
 
     glFlush(); // flush drawing routines to the window
 }
@@ -22,7 +32,11 @@ int main(int argc, char* argv[])
     glutCreateWindow("Hello world"); 
     glutDisplayFunc(display); // callback function
 
-    
+    // set up projection window
+    glMatrixMode(GL_PROJECTION); // selecting projection matrix
+    glLoadIdentity();
+    gluOrtho2D(0, 500, 0, 500);
+    glPointSize(10);
 
     // tell GLUT to wait for the events
     glutMainLoop();
